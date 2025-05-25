6defacae487bd833b48ba1454a2606c851e508c3
Show Wai Yan <showwaiyan555@gmail.com>
2025-05-25 20:30:26 +0800

    Update AppDbContext and Program.cs with PostgreSQL provider

--- Swin-Bite.API/Program.cs
+++ Swin-Bite.API/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using SwinBite.Context;
 
 var builder = WebApplication.CreateBuilder(args);
+// DI IOC Configurations
 
 // Getting Connection String for DbContext to connect the DB
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -9,8 +10,10 @@ if (connectionString == null)
     throw new InvalidOperationException("Connection String: 'DefaultConnection' not found");
 
 // Adding DbContext and connect DbContext and DB
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString)); // Dependency Injection
 var app = builder.Build();
+
+// Middleware Configuration
 
 app.UseHttpsRedirection();
 

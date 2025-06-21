12b7f9548d913a4a220816aac2269f3033ffbda8
Show Wai Yan <showwaiyan555@gmail.com>
2025-06-21 11:30:25 +0800

    Remove CustomerDto from ShoppingCartDto due to object cycle

--- Swin-Bite.API/Dtos/ShoppingCartDto.cs
+++ Swin-Bite.API/Dtos/ShoppingCartDto.cs
@@ -3,7 +3,6 @@ namespace SwinBite.DTO
     public class ShoppingCartDto
     {
         public int ShoppingCartId { get; set; }
-        public CustomerDto Customer { get; set; }
         public List<ShoppingCartItemDto> ShoppingCartItems { get; set; }
     }
 }

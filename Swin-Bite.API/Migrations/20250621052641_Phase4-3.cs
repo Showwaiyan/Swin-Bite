1127784564174a9c06976f3bb58d091b79afde38
Show Wai Yan <showwaiyan555@gmail.com>
2025-06-22 10:53:16 +0800

    Update Phase4-3 Migration

---
+++ Swin-Bite.API/Migrations/20250621052641_Phase4-3.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Swin_Bite.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Phase43 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalQuantity",
+                table: "Foods");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalQuantity",
+                table: "Foods",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Foods",
+                keyColumn: "FoodId",
+                keyValue: 1,
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "Foods",
+                keyColumn: "FoodId",
+                keyValue: 2,
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "Foods",
+                keyColumn: "FoodId",
+                keyValue: 3,
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "Foods",
+                keyColumn: "FoodId",
+                keyValue: 4,
+                columns: new string[0],
+                values: new object[0]);
+        }
+    }
+}

cf1bd8bd89b85f496945b908212566f67983b4f9
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-07 12:57:56 +0800

    Remove private fields from Dish

--- Swin-Bite.API/Models/Dish.cs
+++ Swin-Bite.API/Models/Dish.cs
@@ -1,49 +1,33 @@
+
 namespace SwinBite.Models
 {
     public class Dish : Food
     {
-        // Fields
-        private int _servingSize;
-        private int _spiceLevel;
-        private int _calories;
-        private List<String> _ingredients;
-
-        //Constructor
+        // Constructor
         public Dish()
         {
-          _ingredients = new List<String>();
+            Ingredients = new List<string>();
         }
 
-        // Properites
-        public int ServingSize
-        {
-            get { return _servingSize; }
-            set { _servingSize = value; }
-        }
-        public int SpiceLevel
-        {
-            get { return _spiceLevel; }
-            set { _spiceLevel = value; }
-        }
-        public int Calories
-        {
-            get { return _calories; }
-            set { _calories = value; }
-        }
-        public List<String> Ingredients
-        {
-            get { return _ingredients; }
-            set { _ingredients = value; }
-        }
+        // Properties
+        public int ServingSize { get; set; }
+
+        public int SpiceLevel { get; set; }
+
+        public int Calories { get; set; }
+
+        public List<string> Ingredients { get; set; }
 
         // Methods
         public override string GetDetails()
         {
             string result = "";
-            foreach (string item in Ingredients?? new List<string>())
+
+            foreach (string item in Ingredients ?? new List<string>())
             {
                 result += $"{item},";
             }
+
             return $"{Name} containing {result.TrimEnd(',')} with {Calories} calories and {ServingSize} serving size only for {Price}$";
         }
     }

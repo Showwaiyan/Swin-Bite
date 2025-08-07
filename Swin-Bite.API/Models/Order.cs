7039ff0322103435a4188c32af10529ada742b82
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-07 13:11:19 +0800

    Remove private fields from Order

--- Swin-Bite.API/Models/Order.cs
+++ Swin-Bite.API/Models/Order.cs
@@ -6,128 +6,56 @@ namespace SwinBite.Models
 {
     public class Order : ISubject
     {
-        // Fields
-        private int _orderId;
-        private int _customerId;
-        private Customer _customer;
-        private int _restaurantId;
-        private Restaurant _restaurant;
-        private int? _deliveryDriverId;
-        private DeliveryDriver _deliveryDriver;
-        private List<OrderItem> _orderItems;
-        private decimal _totalPrice;
-        private OrderStatus _status;
-        private OrderType _type;
-        private DateTime _orderDate;
-        private DateTime _pickUpTime;
-
-        private List<IObserver> _observers;
-
         // Constructor
         public Order()
         {
-            _orderItems = new List<OrderItem>();
-            _observers = new List<IObserver>();
+            OrderItems = new List<OrderItem>();
+            Observers = new List<IObserver>();
         }
 
         // Properties
         [Key]
-        public int OrderId
-        {
-            get { return _orderId; }
-            set { _orderId = value; }
-        }
-
-        // Many-To-One Realtionship
-        [Required]
-        public int CustomerId
-        {
-            get { return _customerId; }
-            set { _customerId = value; }
-        }
-
-        [ForeignKey("CustomerId")]
-        public Customer Customer
-        {
-            get { return _customer; }
-            set { _customer = value; }
-        }
+        public int OrderId { get; set; }
 
         // Many-To-One Relationship
         [Required]
-        public int RestaurantId
-        {
-            get { return _restaurantId; }
-            set { _restaurantId = value; }
-        }
+        public int CustomerId { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+        // Many-To-One Relationship
+        [Required]
+        public int RestaurantId { get; set; }
 
         [ForeignKey("RestaurantId")]
-        public Restaurant Restaurant
-        {
-            get { return _restaurant; }
-            set { _restaurant = value; }
-        }
+        public Restaurant Restaurant { get; set; }
 
-        // Many-To-Zero Relationship
-        public int? DeliveryDriverId
-        {
-            get { return _deliveryDriverId; }
-            set { _deliveryDriverId = value; }
-        }
+        // Optional Delivery Driver
+        public int? DeliveryDriverId { get; set; }
 
         [ForeignKey("DeliveryDriverId")]
-        public DeliveryDriver DeliveryDriver
-        {
-            get { return _deliveryDriver; }
-            set { _deliveryDriver = value; }
-        }
+        public DeliveryDriver DeliveryDriver { get; set; }
 
         // One-To-Many Relationship
-        public List<OrderItem> OrderItems
-        {
-            get { return _orderItems; }
-            set { _orderItems = value; }
-        }
+        public List<OrderItem> OrderItems { get; set; }
 
-        public decimal TotalPrice
-        {
-            get { return _totalPrice; }
-            set { _totalPrice = value; }
-        }
+        public decimal TotalPrice { get; set; }
 
-        public OrderStatus Status
-        {
-            get { return _status; }
-            set { _status = value; }
-        }
+        public OrderStatus Status { get; set; }
 
-        public OrderType Type
-        {
-            get { return _type; }
-            set { _type = value; }
-        }
+        public OrderType Type { get; set; }
 
-        public DateTime OrderDate
-        {
-            get { return _orderDate; }
-            set { _orderDate = value; }
-        }
+        public DateTime OrderDate { get; set; }
 
-        public DateTime PickUpTime
-        {
-            get { return _pickUpTime; }
-            set { _pickUpTime = value; }
-        }
+        public DateTime PickUpTime { get; set; }
 
-        public List<IObserver> Observers
-        {
-            get { return _observers; }
-        }
+        public List<IObserver> Observers { get; } // read-only, initialized in constructor
 
         // Methods
         public Notification PlaceOrderNotification()
         {
-            Notification notification = new Notification
+            return new Notification
             {
                 Message =
                     $"Customer {Customer.Username}(ID-{CustomerId}) place order with Order(ID-{OrderId}) at {OrderDate:yyyy-MM-dd HH:mm}",
@@ -136,12 +64,11 @@ namespace SwinBite.Models
                 Type = NotificationType.OrderUpdate,
                 IsRead = false,
             };
-            return notification;
         }
 
         public Notification NewOrderNotification()
         {
-            Notification notification = new Notification()
+            return new Notification
             {
                 Message =
                     $"Order(Id={OrderId}) from {Restaurant.Name} is placed for delivery, from {Restaurant.Address} to {Customer.Address}",
@@ -149,12 +76,11 @@ namespace SwinBite.Models
                 Type = NotificationType.OrderUpdate,
                 IsRead = false,
             };
-            return notification;
         }
 
         public Notification AcceptOrderNotification()
         {
-            Notification notification = new Notification()
+            return new Notification
             {
                 Message =
                     $"Driver {DeliveryDriver.Username} accept Order(Id-{OrderId}) and currently on the way.",
@@ -163,13 +89,11 @@ namespace SwinBite.Models
                 Type = NotificationType.DeliveryUpdate,
                 IsRead = false,
             };
-
-            return notification;
         }
 
         public Notification DeliveredOrderNotification()
         {
-            Notification notification = new Notification()
+            return new Notification
             {
                 Message =
                     $"Delivery {DeliveryDriver.Username} has delivered Order (Id-{OrderId}) at {Customer.Address}",
@@ -178,13 +102,11 @@ namespace SwinBite.Models
                 Type = NotificationType.DeliveryUpdate,
                 IsRead = false,
             };
-
-            return notification;
         }
 
         public Notification CompleteOrderNotification()
         {
-            Notification notification = new Notification()
+            return new Notification
             {
                 Message = $"Customer {Customer.Username} picked up order.",
                 UserId = RestaurantId,
@@ -192,13 +114,11 @@ namespace SwinBite.Models
                 Type = NotificationType.DeliveryUpdate,
                 IsRead = false,
             };
-
-            return notification;
         }
 
         public Notification UpdateStatusNotification()
         {
-            Notification notification = new Notification
+            return new Notification
             {
                 Message = $"Order #{OrderId} status updated to {Status}",
                 UserId = CustomerId,
@@ -206,7 +126,6 @@ namespace SwinBite.Models
                 Type = NotificationType.OrderUpdate,
                 IsRead = false,
             };
-            return notification;
         }
 
         public void UpdateStatus(OrderStatus status)
@@ -221,26 +140,27 @@ namespace SwinBite.Models
 
         public void AddObserver(IObserver observer)
         {
-            if (!_observers.Contains(observer))
+            if (!Observers.Contains(observer))
             {
-                _observers.Add(observer);
+                Observers.Add(observer);
             }
         }
 
         public void RemoveObserver(IObserver observer)
         {
-            if (!_observers.Contains(observer))
+            if (Observers.Contains(observer))
             {
-                _observers.Remove(observer);
+                Observers.Remove(observer);
             }
         }
 
         public void NotifyObservers(Notification notification)
         {
-            foreach (IObserver observer in _observers)
+            foreach (IObserver observer in Observers)
             {
                 observer.Update(notification);
             }
         }
     }
 }
+

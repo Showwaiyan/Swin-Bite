5f443a1c8ced120efc586d3597381adb21ad7cd6
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-07 13:01:17 +0800

    Remove private fields from Drink

--- Swin-Bite.API/Models/Drink.cs
+++ Swin-Bite.API/Models/Drink.cs
@@ -2,42 +2,25 @@ namespace SwinBite.Models
 {
     public class Drink : Food
     {
-        // Fields
-        private int _volume;
-        private int _temperature;
-        private bool _isCarbonated;
-        private bool _hasAlcohol;
-
         // Properties
-        public int Volume
-        {
-            get { return _volume; }
-            set { _volume = value; }
-        }
-        public int Temperature
-        {
-            get { return _temperature; }
-            set { _temperature = value; }
-        }
-        public bool IsCarbonated
-        {
-            get { return _isCarbonated; }
-            set { _isCarbonated = value; }
-        }
-        public bool HasAlcohol
-        {
-            get { return _hasAlcohol; }
-            set { _hasAlcohol = value; }
-        }
+        public int Volume { get; set; }
+
+        public int Temperature { get; set; }
+
+        public bool IsCarbonated { get; set; }
+
+        public bool HasAlcohol { get; set; }
 
         // Methods
         public override string GetDetails()
         {
             string carbonated = IsCarbonated ? "Carbonated" : "Non-Carbonated";
             string alcohol = HasAlcohol
-                ? "Be aware this is non-hala due to containg alcohol"
-                : "Don't worry, this is hala drink, alcohol free!.";
-            return $"{carbonated} {Name} drink only {Price}$ for {Volume} Liters.Only consume at {Temperature}.{alcohol}";
+                ? "Be aware this is non-hala due to containing alcohol"
+                : "Don't worry, this is hala drink, alcohol-free!";
+            
+            return $"{carbonated} {Name} drink only {Price}$ for {Volume} Liters. Only consume at {Temperature}. {alcohol}";
         }
     }
 }
+

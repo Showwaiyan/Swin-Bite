4eca4c12705faa87918792e262def9ef0cd19715
Show Wai Yan <showwaiyan555@gmail.com>
2025-08-07 13:32:57 +0800

    Remove private fields from Snack

--- Swin-Bite.API/Models/Snack.cs
+++ Swin-Bite.API/Models/Snack.cs
@@ -2,39 +2,14 @@ namespace SwinBite.Models
 {
     public class Snack : Food
     {
-        // Fields
-        private int _packageSize;
-        private bool _isHealthy;
-        private List<string> _allergens;
+        public int PackageSize { get; set; }
+        public bool IsHealthy { get; set; }
+        public List<string> Allergens { get; set; } = new();
 
-        //Constructor
-        public Snack()
-        {
-            _allergens = new List<string>();
-        }
-
-        // Properties
-        public int PackageSize
-        {
-            get { return _packageSize; }
-            set { _packageSize = value; }
-        }
-        public bool IsHealthy
-        {
-            get { return _isHealthy; }
-            set { _isHealthy = value; }
-        }
-        public List<string> Allergens
-        {
-            get { return _allergens; }
-            set { _allergens = value; }
-        }
-
-        // Methods
         public override string GetDetails()
         {
-            string allergens = Allergens.Count() > 0 ? "Please aware for allergics reaction" : "";
-            return $"{Name} for {PackageSize}, good for desserts.{allergens}";
+            string allergensNotice = Allergens.Count > 0 ? " Please be aware of potential allergens." : "";
+            return $"{Name} for {PackageSize}, good for desserts.{allergensNotice}";
         }
     }
 }
